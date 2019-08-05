0813fd430b56b0a59d14ea756a03f51c0a15bff1
sun <sunjiud@foxmail.com>
2019-08-06 00:00:41 +0800

    uppdate

--- src/main/java/cn/sun/tasks/task/dao/TaskDao.java
+++ src/main/java/cn/sun/tasks/task/dao/TaskDao.java
@@ -2,6 +2,8 @@ package cn.sun.tasks.task.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Repository;
 
 import cn.sun.tasks.task.domain.Task;
@@ -13,7 +15,7 @@ public interface TaskDao {
 	 * 获取所有任务
 	 * @return
 	 */
-	public List<Task> getAllTasks(Integer start, Integer offset);
+	public List<Task> getAllTasks(@Param("start")Integer start, @Param("offset")Integer offset);
 	
 	/**
 	 * 、根据id获取任务
